797208011d9f174152eade6f15e91d5641ee716b
mby444 <bimayudha161@gmail.com>
2025-02-07 16:49:03 +0700

    Added env variable

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 
 const app = express();
+const message = process.env.MESSAGE || "Hello!";
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello world!" });
+  res.json({ message });
 });
 
 app.listen(5000, () => console.log("App runing at http://localhost:5000"));
